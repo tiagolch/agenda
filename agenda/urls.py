0af865771e17e3952778bb65d9917b7aa0a249cb
Tiago <tiagolch@hotmail.com>
2020-04-02 17:49:18 -0300

    Trabalhado com templates na tela de inserção de dados e adicionado .gitignore

--- agenda/urls.py
+++ agenda/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from core import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('agenda/', views.lista_eventos, name='lista_eventos'),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
+    path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout', views.logout_user),
 ]
